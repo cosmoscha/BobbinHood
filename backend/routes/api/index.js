76f6a1f764c104b07a71845784b8169a9e52de87
River <cosmos.cha1997@gmail.com>
2021-02-01 03:16:10 -0500

    heroku

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,22 +1,57 @@
-const router = require('express').Router();
-const sessionRouter = require('./session.js');
-const usersRouter = require('./users.js');
-const assetsRouter = require('./assets.js');
-const assetRouter = require('./asset.js');
-const portfolioRouter = require('./portfolio.js');
+const router = require("express").Router();
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+const assetsRouter = require("./assets.js");
+const assetRouter = require("./asset.js");
+const portfolioRouter = require("./portfolio.js");
+const express = require("express");
+const router = express.Router();
+const apiRouter = require("./api");
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
+router.post("/test", function (req, res) {
+  res.json({ requestBody: req.body });
+});
+
+router.use("/api", apiRouter);
+
+router.use("/session", sessionRouter);
+
+router.use("/users", usersRouter);
+
+router.use("/assets", assetsRouter);
+
+router.use("/asset", assetRouter);
+
+router.use("/portfolio", portfolioRouter);
+
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  // Serve the frontend's index.html file at the root route
+  router.get("/", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
   });
 
-  router.use('/session', sessionRouter);
+  // Serve the static assets in the frontend's build folder
+  router.use(express.static(path.resolve("../frontend/build")));
 
-  router.use('/users', usersRouter);
+  // Serve the frontend's index.html file at all other routes NOT starting with /api
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, "../../frontend", "build", "index.html")
+    );
+  });
+}
 
-  router.use('/assets', assetsRouter);
-
-  router.use('/asset', assetRouter);
-
-  router.use('/portfolio', portfolioRouter);
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== "production") {
+  router.get("/api/csrf/restore", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.status(201).json({});
+  });
+}
 
 module.exports = router;
